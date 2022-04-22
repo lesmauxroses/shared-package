b6c3730b3295d64eeb429f929f781c93d62fdcd8
Killian <ksowa@outlook.fr>
2022-04-22 18:22:13 +0200

    (feat): add informationView

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "LesMotsRosesUI",
     platforms: [
-        .iOS(.v13),
+        .iOS(.v13), .macOS(.v11),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
---
+++ Sources/LesMotsRosesUI/Components/Information/InformationView.swift
@@ -0,0 +1,48 @@
+//
+//  InformationView.swift
+//  
+//
+//  Created by Killian Sowa on 22/04/2022.
+//
+
+import SwiftUI
+
+struct InformationView: View {
+    var title: String?
+    var message: String?
+    var buttonText: String?
+    var onButtonTapped: () -> Void
+    
+    var body: some View {
+        ZStack {
+            VStack(spacing: 60) {
+//                Image("illu-oeil").blendMode(.normal)
+                
+                if let title = title {
+                    Text(title)
+                        .foregroundColor(Color.white)
+                        .font(.custom("JosefinSans-Bold", size: 52))
+                }
+                
+                if let message = message {
+                    Text(message)
+                        .foregroundColor(Color.white)
+                        .font(.custom("JosefinSans-Bold", size: 52))
+                }
+                
+                if let buttonText = buttonText {
+                    Button {
+                        onButtonTapped()
+                    } label: {
+                        Text(buttonText)
+                    }
+
+                }
+            }
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.purple)
+            .blendMode(.lighten)
+//            .background(Image("bg-grain"))
+            .ignoresSafeArea()
+    }
+}
