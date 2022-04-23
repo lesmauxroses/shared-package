d7ae5bca71be515647e1cd900fca427875b91e43
Killian <ksowa@outlook.fr>
2022-04-23 17:50:36 +0200

    (fix): improve onboarding

--- Sources/LesMotsRosesUI/Components/Onboarding/OnboardingView.swift
+++ Sources/LesMotsRosesUI/Components/Onboarding/OnboardingView.swift
@@ -15,19 +15,21 @@ public struct OnboardingView: View {
     }
     
     public var body: some View {
-        VStack {
+        HStack {
             ForEach(Array(steps.enumerated()), id: \.element) { index, element in
                 VStack {
                     Text("\(index)")
-                        .padding(50)
+                        .foregroundColor(Color.dark100)
+                        .frame(width: 60, height: 60)
                         .overlay(
-                            RoundedRectangle(cornerRadius: 25)
+                            RoundedRectangle(cornerRadius: 30)
                                 .stroke(Color.purple100, lineWidth: 2)
                         )
                     
                     Image(element.imageName)
                     
                     Text(element.description)
+                        .foregroundColor(Color.dark100)
                         .font(.josefinSans)
                         .padding(20)
                         .overlay(
@@ -37,5 +39,27 @@ public struct OnboardingView: View {
                 }
             }
         }
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView(steps: [
+            OnboardingStep(
+                id: "drag",
+                description: "Place les stickers sur les mots que tu trouves sexistes.",
+                imageName: ""
+            ),
+            OnboardingStep(
+                id: "remove",
+                description: "Si tu veux retirer un sticker, appuie sur celui-ci pour le supprimer.",
+                imageName: ""
+            ),
+            OnboardingStep(
+                id: "drag",
+                description: "Les stickers ajoutés par les autres joueurs seront visibles sur l’écran en face de toi à la fin de l’interaction.",
+                imageName: ""
+            )
+        ]).background(Color.paleBrown100)
     }
 }
