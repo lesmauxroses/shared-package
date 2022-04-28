271ad890fd320912a91ef9381783411cf89c7cce
Killian <ksowa@outlook.fr>
2022-04-28 15:14:21 +0200

    (fix): make BackgroundNoise init public

--- Sources/LesMotsRosesUI/ViewModifiers/NoiseBackground.swift
+++ Sources/LesMotsRosesUI/ViewModifiers/NoiseBackground.swift
@@ -10,6 +10,10 @@ import SwiftUI
 public struct NoiseBackground: ViewModifier {
     let type: NoiseBackgroundType
     
+    public init(type: NoiseBackgroundType) {
+        self.type = type
+    }
+    
     public func body(content: Content) -> some View {
         content
             .background(type == .dark ? Color.dark100 : type == .medium ? Color.purple100 : Color.paleBrown100)
