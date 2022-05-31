db4f716f335074d9c879e96444bbd44e48bed2a9
Killian <ksowa@outlook.fr>
2022-05-31 17:15:19 +0200

    (feat): add images on missionSuccess

--- Sources/LesMotsRosesUI/Components/MissionSuccess/MissionSuccessView.swift
+++ Sources/LesMotsRosesUI/Components/MissionSuccess/MissionSuccessView.swift
@@ -17,19 +17,35 @@ public struct MissionSuccessView: View {
     }
     
     public var body: some View {
-        VStack(spacing: 115) {
-            Image("title_missionsuccess")
-            
-            HStack(spacing: 30) {
-                Image("illu_qrcode\(qrCodeNumber)")
+        ZStack {
+            HStack {
+                Image("bgpattern_missionsuccess_left")
                 
-                Text("Bravo vous avez gagné un morceau de QRCode !")
-                    .font(.josefinSansTitle1)
-                    .foregroundColor(Color.dark100)
-            }.frame(maxWidth: 820)
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .modifier(NoiseBackground(type: .light))
+                Spacer()
+                
+                VStack {
+                    Spacer()
+                    
+                    Image("bgpattern_missionsuccess_right")
+                }
+                .frame(maxHeight: .infinity)
+            }
+            .frame(maxWidth: .infinity)
+            
+            VStack(spacing: 80) {
+                Image("title_missionsuccess")
+                
+                HStack(spacing: 30) {
+                    Image("illu_qrcode\(qrCodeNumber)")
+                    
+                    Text("Bravo vous avez gagné un morceau de QRCode !")
+                        .font(.josefinSansTitle1)
+                        .foregroundColor(Color.dark100)
+                }.frame(maxWidth: 820)
+            }
+
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
+            .modifier(NoiseBackground(type: .light))
     }
 }
 
