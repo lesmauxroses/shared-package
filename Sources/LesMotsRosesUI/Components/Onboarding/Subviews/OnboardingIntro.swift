f3a848043fa5813c153ef742dfd6cca2f482bbc9
Killian <ksowa@outlook.fr>
2022-06-17 00:43:58 +0200

    (feat): animate onboardingIntro

--- Sources/LesMotsRosesUI/Components/Onboarding/Subviews/OnboardingIntro.swift
+++ Sources/LesMotsRosesUI/Components/Onboarding/Subviews/OnboardingIntro.swift
@@ -8,12 +8,16 @@
 import SwiftUI
 
 struct OnboardingIntro: View {
+    @State var bouncing: Bool = false
+    
     var body: some View {
         VStack {
             Spacer()
             
             Image("illu_eye_big_dark")
                 .padding(.bottom, 30)
+                .offset(y: bouncing ? -40 : 0)
+                .animation(Animation.easeInOut(duration: 2.0).repeatForever(autoreverses: true))
             
             Image("title_welcome")
                 .padding(.bottom, 120)
@@ -22,6 +26,9 @@ struct OnboardingIntro: View {
                 .font(.josefinSansTitle1)
                 .foregroundColor(Color.dark100)
                 .padding(.bottom, 75)
+        }
+        .onAppear {
+            self.bouncing.toggle()
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .ignoresSafeArea()
