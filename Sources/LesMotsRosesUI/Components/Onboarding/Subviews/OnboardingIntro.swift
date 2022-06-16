d2978d619481018b9e881f617032a4ba942f003d
Killian <ksowa@outlook.fr>
2022-06-17 00:47:32 +0200

    (fix): remove animation offset

--- Sources/LesMotsRosesUI/Components/Onboarding/Subviews/OnboardingIntro.swift
+++ Sources/LesMotsRosesUI/Components/Onboarding/Subviews/OnboardingIntro.swift
@@ -16,7 +16,7 @@ struct OnboardingIntro: View {
             
             Image("illu_eye_big_dark")
                 .padding(.bottom, 30)
-                .offset(y: bouncing ? -40 : 0)
+                .offset(y: bouncing ? -30 : 0)
                 .animation(Animation.easeInOut(duration: 2.0).repeatForever(autoreverses: true))
             
             Image("title_welcome")
