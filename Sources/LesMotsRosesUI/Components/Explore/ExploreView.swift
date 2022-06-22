8124d0e33cdc487b613d1078b7152bd0e842eab5
Killian <ksowa@outlook.fr>
2022-06-22 17:04:03 +0200

    (fix): exploreView ok

--- Sources/LesMotsRosesUI/Components/Explore/ExploreView.swift
+++ Sources/LesMotsRosesUI/Components/Explore/ExploreView.swift
@@ -10,49 +10,29 @@ import SwiftUI
 public struct ExploreView: View {
     var percentImage: String?
     var percentText: String?
+    var text: String
     
     public init(
         percentImage: String? = nil,
-        percentText: String? = nil
+        percentText: String? = nil,
+        text: String = "les décors autour de vous avant de passer à la suite"
     ) {
         self.percentImage = percentImage
         self.percentText = percentText
+        self.text = text
     }
     
     public var body: some View {
         ZStack {
             VStack(spacing: 200) {
-                
-                
                 VStack(spacing: 30) {
                     Image("title_explore")
                     
-                    Text("les décors autour de vous avant de passer à la suite")
+                    Text(text)
                         .foregroundColor(Color.paleBrown100)
                         .font(.custom("JosefinSans-Bold", size: 38))
                 }
-                
-                if(percentImage != nil && self.percentText != nil) {
-                    
-                    ZStack {
-                        Text(self.percentText!)
-                            .foregroundColor(Color.paleBrown100)
-                            .font(.josefinBody)
-                            .frame(width: 500)
-                            .padding(40)
-                            .padding(.top, 50)
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 20)
-                                    .stroke(Color.paleBrown100, lineWidth: 2)
-                                    .rotationEffect(.degrees(-2))
-                            )
-                        
-                        Image(self.percentImage!)
-                            .offset(y: -130)
-                    }
-                }
             }
-            
         }.frame(maxWidth: .infinity, maxHeight: .infinity)
             .modifier(NoiseBackgroundWithPattern(theme: .dark, pattern: .explore))
     }
