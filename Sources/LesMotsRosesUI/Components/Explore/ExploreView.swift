247770e9b16e26eb0611f96926b3d52771338760
Killian <ksowa@outlook.fr>
2022-06-22 17:28:26 +0200

    (fix): hasQr fixed

--- Sources/LesMotsRosesUI/Components/Explore/ExploreView.swift
+++ Sources/LesMotsRosesUI/Components/Explore/ExploreView.swift
@@ -11,27 +11,32 @@ public struct ExploreView: View {
     var percentImage: String?
     var percentText: String?
     var text: String
+    var hasQr: Bool
     
     public init(
         percentImage: String? = nil,
         percentText: String? = nil,
-        text: String = "les décors autour de vous avant de passer à la suite"
+        text: String = "les décors autour de vous avant de passer à la suite",
+        hasQr: Bool = false
     ) {
         self.percentImage = percentImage
         self.percentText = percentText
         self.text = text
+        self.hasQr = hasQr
     }
     
     public var body: some View {
-        ZStack {
-            VStack(spacing: 200) {
-                VStack(spacing: 30) {
-                    Image("title_explore")
-                    
-                    Text(text)
-                        .foregroundColor(Color.paleBrown100)
-                        .font(.custom("JosefinSans-Bold", size: 38))
-                }
+        VStack(spacing: 60) {
+            VStack(spacing: 30) {
+                Image("title_explore")
+                
+                Text(text)
+                    .foregroundColor(Color.paleBrown100)
+                    .font(.custom("JosefinSans-Bold", size: 38))
+            }
+            
+            if hasQr {
+                Image("final_qrcode")
             }
         }.frame(maxWidth: .infinity, maxHeight: .infinity)
             .modifier(NoiseBackgroundWithPattern(theme: .dark, pattern: .explore))
